614754ffa9a1b290b9e5a4abde510381f0462e8f
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-05-30 16:04:22 -0300

    Se agrega el generados de ejemplos y los archivos que usaremos para hacer medidas

---
+++ TP2_Problema_del_Empaquetamiento/generador_de_casos.py
@@ -0,0 +1,13 @@
+import random
+
+def write_random_numbers(filename, num_numbers):
+    with open(filename, 'w') as file:
+        file.write("{}".format(num_numbers) + '\n\n')
+
+        for _ in range(num_numbers):
+            random_number = round(random.random(), 3)
+            file.write(str(random_number) + '\n')
+
+for i in range(1, 11):
+    for j in range(1, 4):
+        write_random_numbers("archivos_prueba/random_numbers_amount_{}_version_{}.txt".format(i, j), i)
